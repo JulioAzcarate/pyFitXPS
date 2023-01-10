c460fb3fc4f3b90f5ab15360bae88ab70c0fa502
JulioAzcarate <jcazcarate@gmail.com>
2023-01-10 15:37:04 -0300

    improves in loading several data files

--- functions/specs_xy.py
+++ functions/specs_xy.py
@@ -30,13 +30,13 @@ from numpy import genfromtxt
 def list_files_xy(path="samefolder"):
     """Función para cargar archivos dentro de una carpeta."""
     if path == "samefolder":
-        list_XPS_files = glob.glob("*.xy")
+        XPS_files = glob.glob("*.xy")
     else:
-        list_XPS_files = glob.glob(path + "/*.xy")
+        XPS_files = glob.glob(path + "/*.xy")
         print(path)
     # creo lista con los nombres de los archivos ordenados alfabéticamente
-    list_XPS_files.sort()
-    print(list_XPS_files)
+    list_XPS_files = list(XPS_files)
+    return list_XPS_files.sort()
 
 
 def which_subfolders(dirname):
@@ -45,7 +45,7 @@ def which_subfolders(dirname):
     """
     subfolders= [f.path for f in os.scandir(dirname) if f.is_dir()]
     for dirname in list(subfolders):
-        subfolders.extend(fast_scandir(dirname))
+        subfolders.extend(which_subfolders(dirname))
     return subfolders
 
 
@@ -145,24 +145,19 @@ def load_one_file_xy(filename):
     return dict_xy
 
 
-def region_to_variable(__name__, dict_name):
-    conv_str_var = sys.modules[__name__]
-    setattr(conv_str_var, # _objet
-        dict_name['details']['region'], # _name: str ... det_dic['region'] = 'region_name'
-        dict_name, # __value: new assigned value to the variable "name"
-        )
-
 def load_all_file_xy_in(folder_path):
     """
-    Read one .xy file and store info in a dictionary
+    Read all files .xy in the folder. Each file in converted in a 
+    dictionary, and stored all together as elements of a dictionary.
 
     Parameters
     ----------
-    filename : TYPE
+    folder_path : TYPE str
         DESCRIPTION.
 
     Returns
     -------
+    Dict which each element is a dictionary
     dict with two elements: 'details' and 'data_orig':
         'details' is a dict wich store the info from adquisition parameters
         'data_orig' dict which contain tuples for orinigal data as two elements:
@@ -179,86 +174,12 @@ def load_all_file_xy_in(folder_path):
     
     files = os.listdir(folder_path)
 
-    list_of_regions = []
+    exper_dict = {}  
 
     for filename in files:
-        file = open(folder_path + filename, 'r')
+        dict_region = load_one_file_xy(folder_path + filename)
+        exper_dict.update({dict_region['details']['Region']:dict_region})
     
-        dict_details = {} # create the emty dict to store the details of experiment
-        
-        details_par = ( # parameters to read from comment lines in .xy file
-            'Region',
-            'Acquisition Date',
-            'Analysis Method',
-            'Analyzer',
-            'Analyzer Lens',
-            'Analyzer Slit',
-            'Scan Mode',
-            'Curves/Scan',
-            'Values/Curve',
-            'Dwell Time',
-            'Excitation Energy',
-            'Binding Energy',
-            'Pass Energy',
-            'Bias Voltage',
-            'Detector Voltage',
-            'Eff. Workfunction',
-            'Source',
-            'Comment',
-            'Number of Scans',
-            )
-        
-        for x in file:
-            if '# ' in x:
-                z = x.replace("# ", "")
-                (k,v) = z.split(":", maxsplit=1) # splir the string after the first ":"
-                if k in details_par: # just take the listed keys
-                    dict_details.update({k.strip(): v.strip()}) # add the k and v to dict
-    
-        dict_xy = {'details': dict_details, 
-                   } # create the final dict
-        
-        # key for parameter values to be converted to each proper type
-        par_float = ('Dwell Time','Excitation Energy','Binding Energy','Pass Energy',
-        'Bias Voltage','Detector Voltage','Eff. Workfunction','Number of Scans',)
-        par_int = ('Curves/Scan','Values/Curve','Number of Scans')
-        
-        for keys in par_float:
-            dict_xy['details'][keys] = float(dict_xy['details'][keys])
-        for keys in par_int:
-            dict_xy['details'][keys] = int(dict_xy['details'][keys])
-        
-        # closing the opened file
-        file.close()
-        
-        # read file with numpy.genfromtxt and create one numpy.array pear each column
-        # and store them as "unwritable" np.array.
-        BE, intensity = genfromtxt(folder_path + filename, comments='#', unpack=True)
-        BE = BE * (-1)
-        KE = dict_xy['details']['Excitation Energy'] + BE
-        
-        # convert BE, KE, intensity for original data as "unwritable" to perserve it
-        BE.setflags(write=False)
-        KE.setflags(write=False)
-        intensity.setflags(write=False)
-        
-        data = {}
-        data.update({'BE':BE})
-        data.update({'KE':KE})
-        data.update({'intensity':intensity})
-        
-        dict_xy.update({'data_orig': data})
-    
-        conv_str_var = sys.modules[__name__]
-        region_name = dict_xy['details']['Region']
-        new_region_name = region_name.replace(" (", "_").replace(")","")
-        
-        j = setattr(conv_str_var, # _objet
-                    new_region_name, # _name: str ... det_dic['region'] = 'region_name'
-                    dict_xy, # __value: new assigned value to the variable "name"
-                    )
-        
-        list_of_regions.append(j)
-    
-    return list_of_regions
+    return exper_dict
+
     
